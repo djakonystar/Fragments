e9b8a46ee99b81f6642b3829b0a6d6d766db37c8
Jaksibay Khakimov <djakonystar@MacBook-Pro-Jaksibay.local>
2022-06-25 09:59:41 +0500

    Working with BackStack

--- app/src/main/java/uz/texnopos/fragments/MainActivity.kt
+++ app/src/main/java/uz/texnopos/fragments/MainActivity.kt
@@ -12,12 +12,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, FirstFragment())
+            .addToBackStack(FirstFragment::class.java.simpleName + "$")
+            .commit()
+
         binding.btnFirst.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, FirstFragment()).commit()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, FirstFragment())
+                .addToBackStack(FirstFragment::class.java.simpleName)
+                .commit()
         }
 
         binding.btnSecond.setOnClickListener {
-            supportFragmentManager.beginTransaction().replace(R.id.fragment_container, SecondFragment()).commit()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, SecondFragment())
+                .addToBackStack(SecondFragment::class.java.simpleName)
+                .commit()
         }
+
+        binding.btnBack.setOnClickListener {
+            val count = supportFragmentManager.backStackEntryCount
+            if (count == 1) {
+                val name = supportFragmentManager.getBackStackEntryAt(0).name
+                if (name == FirstFragment::class.java.simpleName + "$") {
+                    finish()
+                }
+            }
+            supportFragmentManager.popBackStack()
+        }
+    }
+
+    override fun onBackPressed() {
+        if (supportFragmentManager.getBackStackEntryAt(0).name == FirstFragment::class.java.simpleName + "$") finish()
+        super.onBackPressed()
     }
 }
